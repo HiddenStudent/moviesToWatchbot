733ef8bb882eb26ba9e5b8ff8fd18e037a859f6e
Yevhenii <oleshkoevgeniy@gmail.com>
2020-05-23 12:41:58 +0300

    rm faker

--- Gemfile
+++ Gemfile
@@ -7,5 +7,3 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "telegram-bot-ruby"
-
-gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
--- main.rb
+++ main.rb
@@ -12,7 +12,7 @@ Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
     else
       bot.api.send_message(
           chat_id: message.chat.id,
-          text: Faker::Movie.quote
+          text: '501 NOT IMPLEMENTED!'
       )
     end
   end
